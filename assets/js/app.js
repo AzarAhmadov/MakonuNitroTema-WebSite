92a0e3950353a3723274b5adc77886c89900a003
azerahmedov <ehmedovh111@gmail.com>
2021-12-10 22:04:37 +0400

    elaveler

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,16 @@
+const navLink = document.querySelectorAll('.h-link');
+
+for(let i = 0 ; i<navLink.length; i++){
+  navLink[i].addEventListener('click', (e) => {
+    document.querySelector(".h-link.current_page_item").classList.remove("current_page_item");
+    navLink[i].classList.add("current_page_item");
+  })
+}
+
 var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
       dynamicBullets: true,
     },
-  });
+});
+
