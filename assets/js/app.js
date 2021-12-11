3344b376e6f0b149e35d957c71eb5681683cdeac
azerahmedov <ehmedovh111@gmail.com>
2021-12-11 14:00:15 +0400

    services elave edildi

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
 const navLink = document.querySelectorAll('.h-link');
 
-for(let i = 0 ; i<navLink.length; i++){
-  navLink[i].addEventListener('click', (e) => {
-    document.querySelector(".h-link.current_page_item").classList.remove("current_page_item");
-    navLink[i].classList.add("current_page_item");
-  })
-}
+navLink.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    document.querySelector(".current_page_item").classList.remove("current_page_item");
+    item.classList.add("current_page_item");  
+  });
+});
 
 var swiper = new Swiper(".mySwiper", {
     pagination: {
