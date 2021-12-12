43a30681662350430c4fd3af7a069e86d4148301
azerahmedov <ehmedovh111@gmail.com>
2021-12-12 23:39:16 +0400

    elaveler

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,15 @@
+const acardionItem = document.querySelectorAll('.acardion-item');
+const acardionContent = document.querySelectorAll('.acardion-content');
+const upICon = document.querySelectorAll('.fa-angle-up');
+
+for(let i = 0; i<acardionItem.length && acardionContent.length && upICon.length; i++){
+  acardionItem[i].addEventListener('click', () => {
+    acardionItem[i].classList.toggle('active');
+    acardionContent[i].classList.toggle('active');
+    upICon[i].classList.toggle('active')
+  });
+};
+
 const navLink = document.querySelectorAll('.h-link');
 
 navLink.forEach((item) => {
