6c79033673ae92300a678860b86911027a990a9a
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2023-03-16 17:16:05 +0400

    finish

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,17 @@
+const listMobil = document.querySelector('.listMobil');
+const bars = document.querySelector('.fa-bars');
+const body = document.querySelector('#body');
+bars.addEventListener('click', () => {
+  bars.classList.toggle('fa-x')
+  listMobil.classList.toggle("active")
+  body.classList.toggle('active')
+})
+
 const acardionItem = document.querySelectorAll('.acardion-item');
 const acardionContent = document.querySelectorAll('.acardion-content');
 const upICon = document.querySelectorAll('.fa-angle-up');
 
-for(let i = 0; i<acardionItem.length && acardionContent.length && upICon.length; i++){
+for (let i = 0; i < acardionItem.length && acardionContent.length && upICon.length; i++) {
   acardionItem[i].addEventListener('click', () => {
     acardionItem[i].classList.toggle('active');
     acardionContent[i].classList.toggle('active');
@@ -15,14 +24,13 @@ const navLink = document.querySelectorAll('.h-link');
 navLink.forEach((item) => {
   item.addEventListener("click", (e) => {
     document.querySelector(".current_page_item").classList.remove("current_page_item");
-    item.classList.add("current_page_item");  
+    item.classList.add("current_page_item");
   });
 });
 
 var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      dynamicBullets: true,
-    },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
 });
-
